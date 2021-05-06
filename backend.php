53b144130a3a76623fad199a7d2f80bb9956d42f
elber jose <elberjsn@uni9.edu.br>
2021-05-06 07:11:55 -0300

    modified:   backend.php

--- backend.php
+++ backend.php
@@ -17,7 +17,7 @@ try {
     $conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
+
     }
 catch(PDOException $e)
     {
