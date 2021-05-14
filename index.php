2817fb6fe0bb9c30b44704c43865f414ea909f44
elber jose <elberjsn@uni9.edu.br>
2021-05-14 14:56:29 -0300

    Bugs 14-05

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 	session_start();
 	if(!isset($_SESSION['id_user'])){
-		header("Location: /user/");
+		header("Location: user/");
 		die();
 	} 
  ?>
--- user/index.php
+++ user/index.php
@@ -4,7 +4,7 @@
 		header("Location: ../");
 		die();
 	} 
- ?>
+?>
 <!DOCTYPE html>
 <html>
 <head>
